0ca8ba6d4271ef7c2493dc3c24814e9aba2ce466
Juan Pablo Sanchez <Juan Pablo Sanchez@DESKTOP-U4HB8MQ>
2023-06-14 11:46:09 -0500

    ahi vamos ingresando datos y poniendo el menu para mobiles

--- dist/main.js
+++ dist/main.js
@@ -1,14 +1,31 @@
 const $ = (el) => document.getElementById(el);
 
+const getDarkMode = () => window.localStorage.getItem('darkMode') || window.localStorage.setItem('darkMode','false');
 
 const setDarkMode = $("dark-mode");
 
 const header = $("header");
 
-
 setDarkMode.addEventListener("change", (e) => {
-  document.documentElement.classList.toggle("dark");
+  document.documentElement.classList.toggle("dark")
+  setDarkMode.checked = !setDarkMode.checked
+  if(document.documentElement.classList.contains('dark')){
+    localStorage.setItem('darkMode','true')
+    setDarkMode.checked = true
+  }else{
+    localStorage.setItem('darkMode','false')
+    setDarkMode.checked = false
+  }
 });
+
+if (localStorage.getItem('darkMode') === 'true'){
+  document.documentElement.classList.add('dark')
+  setDarkMode.checked = true
+
+}else{
+  document.documentElement.classList.remove('dark')
+  setDarkMode.checked = false
+}
 
 let firtsLocation = window.pageYOffset;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: "class",
-  theme: {},
+  theme: {
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+      xs: {'max': '700px'}
+    },
+  },
   extend: {
     // that is animation class
     animation: {},
