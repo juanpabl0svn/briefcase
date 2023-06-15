9c1c059b0899f50ebc3103cb78f4af7cd1e032cc
Juan Pablo Sanchez <Juan Pablo Sanchez@DESKTOP-U4HB8MQ>
2023-06-15 02:30:42 -0500

    mejoramos diseño e interaccion

--- dist/main.js
+++ dist/main.js
@@ -1,40 +1,43 @@
 const $ = (el) => document.getElementById(el);
 
-const getDarkMode = () => window.localStorage.getItem('darkMode') || window.localStorage.setItem('darkMode','false');
+const getDarkMode = () =>
+  window.localStorage.getItem("darkMode") ||
+  window.localStorage.setItem("darkMode", "false");
 
 const setDarkMode = $("dark-mode");
 
 const header = $("header");
 
+const mobile = $("mobile");
+
 setDarkMode.addEventListener("change", (e) => {
-  document.documentElement.classList.toggle("dark")
-  setDarkMode.checked = !setDarkMode.checked
-  if(document.documentElement.classList.contains('dark')){
-    localStorage.setItem('darkMode','true')
-    setDarkMode.checked = true
-  }else{
-    localStorage.setItem('darkMode','false')
-    setDarkMode.checked = false
+  document.documentElement.classList.toggle("dark");
+  setDarkMode.checked = !setDarkMode.checked;
+  if (document.documentElement.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "true");
+    setDarkMode.checked = true;
+  } else {
+    localStorage.setItem("darkMode", "false");
+    setDarkMode.checked = false;
   }
 });
 
-if (localStorage.getItem('darkMode') === 'true'){
-  document.documentElement.classList.add('dark')
-  setDarkMode.checked = true
-
-}else{
-  document.documentElement.classList.remove('dark')
-  setDarkMode.checked = false
+if (localStorage.getItem("darkMode") === "true") {
+  document.documentElement.classList.add("dark");
+  setDarkMode.checked = true;
+} else {
+  document.documentElement.classList.remove("dark");
+  setDarkMode.checked = false;
 }
 
 let firtsLocation = window.pageYOffset;
 
-window.onscroll = function () {
-  if (window.scrollY === 0){
-    header.style.top = '0'
-    return
+document.onscroll = function () {
+  if (window.scrollY === 0 || mobile.checked) {
+    header.style.top = "0";
+    return;
   }
-  // if (window.scrollY)
+
   let displacement = window.pageYOffset;
   header.style.top = firtsLocation >= displacement ? "0" : "-100px";
   firtsLocation = displacement;
